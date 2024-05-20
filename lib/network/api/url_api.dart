7b8a8ea0d21bcb6b7e907dc6818e8410ed6f2fba
davarezza <dapva2728@gmail.com>
2024-05-20 22:25:01 +0700

    feat: completed display for get cart product using API

--- lib/network/api/url_api.dart
+++ lib/network/api/url_api.dart
@@ -4,5 +4,6 @@ class BASEURL {
   static String apiGetProductWithCategory = "http://localhost/medhealth_db/get_product_with_category.php";
   static String apiGetProduct = "http://localhost/medhealth_db/get_product.php";
   static String addToCart = "http://localhost/medhealth_db/add_to_cart.php";
+  static String getProductCart = "http://localhost/medhealth_db/get_cart.php?userID=";
 }
 
